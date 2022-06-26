1d0da6276b5880c479ae0d06cd46228b6a2404b9
Hery <thitphavanh.psv@gmail.com>
2022-06-26 18:32:35 +0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
-
+import {
+  getFirestore,
+  collection,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyARgMB4aW-MywP5Kf1vQpGU5v9Yy750HQY",
   authDomain: "basic-firebase-da1ef.firebaseapp.com",
@@ -12,3 +16,24 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const table = document.getElementById("table");
+
+async function getEmployees(db) {
+  const empCol = collection(db, "employees");
+  const empSnapshot = await getDocs(empCol);
+  return empSnapshot;
+}
+
+function showData(employee) {
+  const row = table.insertRow(-1);
+  const nameCol = row.insertCell(0);
+  const ageCol = row.insertCell(1);
+  nameCol.innerHTML = employee.data().name;
+  ageCol.innerHTML = employee.data().age;
+}
+
+const data = await getEmployees(db);
+data.forEach((employee) => {
+  showData(employee);
+});
