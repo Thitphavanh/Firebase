1ef10ad38e623ce7a47748613cc927e3beaec816
Hery <thitphavanh.psv@gmail.com>
2022-06-26 17:44:01 +0700

    update

---
+++ app.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARgMB4aW-MywP5Kf1vQpGU5v9Yy750HQY",
+  authDomain: "basic-firebase-da1ef.firebaseapp.com",
+  projectId: "basic-firebase-da1ef",
+  storageBucket: "basic-firebase-da1ef.appspot.com",
+  messagingSenderId: "362670116743",
+  appId: "1:362670116743:web:723de4041b4be54e444d22",
+  measurementId: "G-8GH07EZ9Y3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
