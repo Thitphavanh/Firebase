f8e636129883e7d8e04644c4f70a720bdf8d2356
Hery <thitphavanh.psv@gmail.com>
2022-08-07 20:24:46 +0700

    update

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ import {
   getFirestore,
   collection,
   getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
 } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyARgMB4aW-MywP5Kf1vQpGU5v9Yy750HQY",
   authDomain: "basic-firebase-da1ef.firebaseapp.com",
@@ -18,6 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const table = document.getElementById("table");
+const form = document.getElementById("addForm");
 
 async function getEmployees(db) {
   const empCol = collection(db, "employees");
@@ -29,11 +34,35 @@ function showData(employee) {
   const row = table.insertRow(-1);
   const nameCol = row.insertCell(0);
   const ageCol = row.insertCell(1);
+  const deleteCol = row.insertCell(2);
   nameCol.innerHTML = employee.data().name;
   ageCol.innerHTML = employee.data().age;
+
+  // crete delete button
+  let btn = document.createElement("button");
+  btn.textContent = "ລົບຂໍ້ມູນ";
+  btn.setAttribute("class", "btn btn-danger");
+  btn.setAttribute("data-id", employee.id);
+  deleteCol.appendChild(btn);
+  btn.addEventListener("click", (e) => {
+    let id = e.target.getAttribute("data-id");
+    deleteDoc(doc(db, "employees", id));
+  });
 }
 
 const data = await getEmployees(db);
 data.forEach((employee) => {
   showData(employee);
+});
+
+// Get information from form
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addDoc(collection(db, "employees"), {
+    name: form.name.value,
+    age: form.age.value,
+  });
+  from.name.value = "";
+  from.age.value = "";
+  alert("ບັນທຶກຂໍ້ມູນສຳເລັດ");
 });
